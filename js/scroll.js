c29f10480b5eaeb185a9d73888c26b854c529888
Yusufly <123965338+Yusufly@users.noreply.github.com>
2023-10-24 18:27:04 +0300

    footer ve scroll

---
+++ js/scroll.js
@@ -0,0 +1,25 @@
+// Navbar Hızlı Link
+function yuzyil() {
+    window.scrollBy(0,100)
+}
+
+function kisi() {
+    window.scrollBy(0,950)
+}
+
+function tekno() {
+    window.scrollBy(0,1400)
+}
+
+// Footer Hızlı Link
+function yuzyil1() {
+    window.scrollTo(0,100)
+}
+
+function kisi1() {
+    window.scrollTo(0,950)
+}
+
+function tekno1() {
+    window.scrollTo(0,1400)
+}
